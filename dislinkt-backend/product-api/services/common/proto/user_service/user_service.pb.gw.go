db21197a2a3ffa2b01fc3387165c4eaaa4bd51be
LidijaSuvajdzic <74602219+LidijaSuvajdzic@users.noreply.github.com>
2022-07-01 18:00:43 +0200

    fix Change Password:gRPC

--- dislinkt-backend/product-api/services/common/proto/user_service/user_service.pb.gw.go
+++ dislinkt-backend/product-api/services/common/proto/user_service/user_service.pb.gw.go
@@ -185,18 +185,15 @@ func local_request_UserService_FindByUsername_0(ctx context.Context, marshaler r
 
 }
 
-var (
-	filter_UserService_ChangePassword_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_UserService_ChangePassword_0(ctx context.Context, marshaler runtime.Marshaler, client UserServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ChangePasswordRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_UserService_ChangePassword_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.ChangePassword); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -209,10 +206,11 @@ func local_request_UserService_ChangePassword_0(ctx context.Context, marshaler r
 	var protoReq ChangePasswordRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_UserService_ChangePassword_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.ChangePassword); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
