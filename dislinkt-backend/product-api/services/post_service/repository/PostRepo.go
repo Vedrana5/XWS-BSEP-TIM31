cb0557c4dc823e4a34dd60f7f4273ccb60521a59
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-07-02 19:40:58 +0200

    add methods to service

--- dislinkt-backend/product-api/services/post_service/repository/PostRepo.go
+++ dislinkt-backend/product-api/services/post_service/repository/PostRepo.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"post/module/model"
 )
 
 const (
@@ -16,4 +18,32 @@ type PostRepo struct {
 func NewPostRepository(client *mongo.Client) PostRepo {
 	posts := client.Database(DATABASE).Collection(CollectionPost)
 	return PostRepo{posts: posts}
+}
+
+func (r PostRepo) GetById(id primitive.ObjectID) *model.Post {
+	return nil
+}
+
+func (r PostRepo) GetAll() []*model.Post {
+	return nil
+}
+
+func (r PostRepo) GetAllByUsername(username string) []*model.Post {
+	return nil
+}
+
+func (r PostRepo) Create(post *model.Post) error {
+	return nil
+}
+
+func (r PostRepo) CreateComment(post *model.Post, comment *model.Comment) error {
+	return nil
+}
+
+func (r PostRepo) LikePost(post *model.Post, id interface{}) error {
+	return nil
+}
+
+func (r PostRepo) DislikePost(post *model.Post, id interface{}) error {
+	return nil
 }
--- dislinkt-backend/product-api/services/post_service/service/PostService.go
+++ dislinkt-backend/product-api/services/post_service/service/PostService.go
@@ -1,11 +1,44 @@
 package service
 
-import "post/module/repository"
+import (
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"post/module/model"
+	"post/module/repository"
+)
 
 type PostService struct {
-	Repo repository.PostRepo
+	Repo *repository.PostRepo
 }
 
 func NewPostService(repository repository.PostRepo) *PostService {
-	return &PostService{Repo: repository}
+	return &PostService{Repo: &repository}
+}
+
+func (service *PostService) GetById(id primitive.ObjectID) *model.Post {
+	return service.Repo.GetById(id)
+}
+
+func (service *PostService) GetAll() []*model.Post {
+	return service.Repo.GetAll()
+}
+
+func (service *PostService) GetAllByUsername(username string) []*model.Post {
+	return service.Repo.GetAllByUsername(username)
+}
+
+func (service *PostService) Create(post *model.Post) error {
+	return service.Repo.Create(post)
+}
+
+func (service *PostService) CreateComment(post *model.Post, comment *model.Comment) error {
+	return service.Repo.CreateComment(post, comment)
+}
+
+func (service *PostService) LikePost(post *model.Post, userId uuid.UUID) error {
+	return service.Repo.LikePost(post, userId)
+}
+
+func (service *PostService) DislikePost(post *model.Post, userId uuid.UUID) error {
+	return service.Repo.DislikePost(post, userId)
 }
