8b5a2e14df6765dd7e8995563e6698c5da869c55
Vedrana5 <mijatovic.ra58.2018@uns.ac.rs>
2022-07-03 23:01:01 +0200

    add methods to repo for offers

--- dislinkt-backend/product-api/services/post_service/repository/PostRepo.go
+++ dislinkt-backend/product-api/services/post_service/repository/PostRepo.go
@@ -9,12 +9,14 @@ import (
 )
 
 const (
-	DATABASE       = "posts"
-	CollectionPost = "postsData"
+	DATABASE           = "posts"
+	CollectionPost     = "postsData"
+	CollectionJobOffer = "jobOffersData"
 )
 
 type PostRepo struct {
-	posts *mongo.Collection
+	posts     *mongo.Collection
+	jobOffers *mongo.Collection
 }
 
 func NewPostRepository(client *mongo.Client) PostRepo {
@@ -106,6 +108,21 @@ func (r PostRepo) CreateComment(post *model.Post, comment *model.Comment) error 
 	return nil
 }
 
+func (r PostRepo) CreateJobOffer(offer *model.JobOffer) error {
+	result, err := r.jobOffers.InsertOne(context.TODO(), offer)
+	if err != nil {
+		return err
+	}
+	offer.Id = result.InsertedID.(primitive.ObjectID)
+
+	return nil
+}
+
+func (r PostRepo) GetAllJobOffers() ([]*model.JobOffer, error) {
+	filter := bson.D{}
+	return r.filterJobOffers(filter)
+}
+
 func (r PostRepo) Create(post *model.Post) error {
 	result, err := r.posts.InsertOne(context.TODO(), post)
 	if err != nil {
@@ -151,6 +168,31 @@ func (r PostRepo) filter(filter interface{}) ([]*model.Post, error) {
 	}
 
 	return decode(cursor)
+}
+
+func (r PostRepo) filterJobOffers(filter bson.D) ([]*model.JobOffer, error) {
+	cursor, err := r.jobOffers.Find(context.TODO(), filter)
+	defer cursor.Close(context.TODO())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return decodeJobOffers(cursor)
+}
+
+func decodeJobOffers(cursor *mongo.Cursor) (offers []*model.JobOffer, err error) {
+	for cursor.Next(context.TODO()) {
+		var offer model.JobOffer
+		err = cursor.Decode(&offer)
+		if err != nil {
+			return
+		}
+		offers = append(offers, &offer)
+	}
+	err = cursor.Err()
+	return
+
 }
 
 func decode(cursor *mongo.Cursor) (posts []*model.Post, err error) {
